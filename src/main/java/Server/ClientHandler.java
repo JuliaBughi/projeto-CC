6fd30f9978b81d9bbbfeb6e64dabc766c5e62f33
a104532 <a104532@uminho.pt>
2024-11-21 22:37:22 +0000

    Retransmissão do packet nettask

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -3,6 +3,7 @@ package Server;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.util.List;
 import Packet.*;
 
@@ -23,57 +24,88 @@ public class ClientHandler implements Runnable {
             this.clientPort = clientPort;
         }
 
-        @Override
-        public void run() {
-            DatagramSocket responseSocket = null;
-            try {
-                responseSocket = new DatagramSocket();
+    @Override
+    public void run() {
+        DatagramSocket responseSocket = null;
+        try {
+            responseSocket = new DatagramSocket();
 
-                //podemos assumir que é logo a primeira ligação
-                this.server.addDevice(clientAddress, receivePacket.getDevice_id());
 
-                List<Task> tasksForDevice = Task.getTasksForDevice(receivePacket.getDevice_id(), server.getTaskList());
+            //podemos assumir que é logo a primeira ligação
+            this.server.addDevice(clientAddress, receivePacket.getDevice_id());
 
-                if(tasksForDevice.isEmpty()){
-                    // se não houver tasks para mandar fecha-se a ligação?
-                    // provavelmente tinha de mandar um pacote ao cliente a dizer para fechar a ligação
-                    //mete-se o ack=2 para o cliente saber que é para terminar a ligação
-                    NetTaskPacket newPacket = new NetTaskPacket(2, receivePacket.getDevice_id(), 1, tasksForDevice);
-                    String serverResponse = NetTaskPacket.NetTaskPacketToString(newPacket);
-                    byte[] sendData = serverResponse.getBytes();
+            List<Task> tasksForDevice = Task.getTasksForDevice(receivePacket.getDevice_id(), server.getTaskList());
 
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
-                    responseSocket.send(sendPacket);
+            if(tasksForDevice.isEmpty()){
+                // se não houver tasks para mandar fecha-se a ligação?
+                // provavelmente tinha de mandar um pacote ao cliente a dizer para fechar a ligação
+                //mete-se o ack=2 para o cliente saber que é para terminar a ligação
+                NetTaskPacket newPacket = new NetTaskPacket(2, receivePacket.getDevice_id(), 1, tasksForDevice);
+                String serverResponse = NetTaskPacket.NetTaskPacketToString(newPacket);
+                byte[] sendData = serverResponse.getBytes();
 
-                    System.out.println("No tasks to send to client "+ receivePacket.getDevice_id());
-                    System.out.println("Closing connection...");
-                }
-                else{ // aqui o device_id já não importa porque ele já foi registado no map do server
-                    NetTaskPacket newPacket = new NetTaskPacket(1, receivePacket.getDevice_id(), 1, tasksForDevice);
-                    String serverResponse = NetTaskPacket.NetTaskPacketToString(newPacket);
-                    byte[] sendData = serverResponse.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                responseSocket.send(sendPacket);
 
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
-                    responseSocket.send(sendPacket);
+                System.out.println("No tasks to send to client "+ receivePacket.getDevice_id());
+                System.out.println("Closing connection...");
+            }
+            else{
 
-                    System.out.println("Tasks sent to client "+ receivePacket.getDevice_id());
-                    for(Task t : tasksForDevice)
-                        System.out.println("Task "+t.getTask_id());
+                int tentativas = 0;
+                int maxTentativas = 5;
+                boolean ackRecebido = false;
 
-                    while(true){
-                        // ver se recebe um ack(do cliente a dizer que recebeu as tarefas)
-                        // ou se são dados enviados pelo cliente
+                //ciclo para retransmissão do pacote
+                while(tentativas < maxTentativas && !ackRecebido){
+
+                    try{
+                        //envio do pacote
+                        NetTaskPacket newPacket = new NetTaskPacket(1, receivePacket.getDevice_id(), 1, tasksForDevice);
+                        String serverResponse = NetTaskPacket.NetTaskPacketToString(newPacket);
+                        byte[] sendData = serverResponse.getBytes();
+
+                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                        responseSocket.send(sendPacket);
+
+                        System.out.println("Tasks sent to client "+ receivePacket.getDevice_id());
+                        for(Task t : tasksForDevice)
+                            System.out.println("Task "+t.getTask_id());
+
+                        responseSocket.setSoTimeout(5000);
+                        byte[] buffer = new byte[1024];
+                        DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+
+                        responseSocket.receive(receivePacket);
+
+                        String ackResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                        NetTaskPacket receivedPacket = NetTaskPacket.StringToNetTaskPacket(ackResponse);
+
+                        if(receivedPacket.getAck() == 1){
+                            ackRecebido = true;
+                        }
+                    } catch (SocketTimeoutException e) {
+                        tentativas++;
                     }
 
                 }
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (responseSocket != null && !responseSocket.isClosed()) {
-                    responseSocket.close();
+
+
+                //ciclo para coleta das métricas
+                while(true){
+
                 }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (responseSocket != null && !responseSocket.isClosed()) {
+                responseSocket.close();
             }
         }
     }
+}
 
